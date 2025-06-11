1a33657c16a1e207038de05dfbf1810a3582c407
Shashank Yadav <shashankyadav724@gmail.com>
2025-06-11 13:02:45 +0530

    Playing with Route Handlers

--- src/app.js
+++ src/app.js
@@ -5,21 +5,24 @@ app.get("/", (req, res) => {
   res.send("Hello From the server...");
 });
 
-app.get("/test/:userID/:password", (req, res) => {
-  console.log(req.params)
-  res.send("Hello From the Test Route...");
-});
-app.post("/test", (req, res) => {
-  res.send("Hello From the Post Method...");
-});
-
-app.patch("/test", (req, res) => {
-  res.send("Hello From the Patch Method..");
-});
-
-app.delete("/test", (req, res) => {
-  res.send("Hello From the Delete Method...");
-});
+app.get("/test/:userID/:password",(req, res, next) => {
+    console.log(req.params);
+    next();
+  },
+  [(req, res, next) => {
+    console.log("Adding Route handler 1");
+    res.send("Hello From Route handler 1");
+    next();
+  },
+  (req, res, next) => {
+    console.log("Adding Route handler 2");
+    next();
+  }],
+  [(req, res, next) => {
+    console.log("Adding Route handler 3");
+    res.send("Hello From Route handler 3");
+  }],
+);
 
 app.listen(3000, () => {
   console.log("The Server Is Up and Running...");
