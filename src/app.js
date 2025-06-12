9eb70db319633e10bfbb9bdfb2a3d5e46dbf7ab7
Shashank Yadav <shashankyadav724@gmail.com>
2025-06-12 23:56:43 +0530

    Error Handling

--- src/app.js
+++ src/app.js
@@ -1,31 +1,20 @@
 const express = require("express");
 const app = express();
-const { adminAuth, userAuth } = require("./middleware/adminAuth");
-
-app.use("/admin", adminAuth);
-
-app.use("/user", userAuth, (req, res,next) => {
- console.log("Checking User Details");
- next();
-});
 
 app.get("/user/test1", (req, res) => {
-  res.send("user1 Route handler");
-
+  //res.send("user1 Route handler");
+  throw new Error("Possible error");
 });
 
 app.get("/user/test2", (req, res) => {
-  res.send("user2 Route handler");
+  //res.send("user1 Route handler");
+  throw new Error("Possible error occured");
 });
-
-app.get("/admin/test1", (req, res) => {
-  res.send("Test1 Route handler");
-});
-
-app.get("/admin/test2", (req, res) => {
-  res.send("Test2 Route handler");
-});
+//Global Error Handler
+app.use((err,req,res,next)=>{
+  res.status(401).send("Something Went Wrong...!")
+})
 
 app.listen(3000, () => {
-  console.log("The Server Is Up and Running...");
+  console.log("The Server Is Up and Running on port no.3000");
 });
